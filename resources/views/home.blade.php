d08c97ba8d159eeac8d949df6fdb1a740b48da35
AcepImam21 <Acepimam19@gmail.com>
2023-12-31 03:11:17 +0700

    perbaikan carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,9 @@
     </div> --}}
 
     <div class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-40">
-        <h1 class="text-4xl md:text-6xl font-bold mb-4">Selamat Datang di ZenithNews</h1>
-        <p class="text-lg md:text-xl mb-8">Temukan berita terkini dan informasi menarik di seluruh dunia.</p>
-        <a href="/kategori" class="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600">Baca Sekarang</a>
+        <h1 class="text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Selamat Datang di ZenithNews</h1>
+        <p class="text-lg md:text-xl lg:text-2xl mb-8">Temukan berita terkini dan informasi menarik di seluruh dunia.</p>
+        <a href="/Kkategori" class="bg-blue-500 text-white px-6 py-2 rounded-full hover:bg-blue-600 text-base md:text-lg lg:text-xl">Baca Sekarang</a>
     </div>
 
     {{-- Carousel --}} 
